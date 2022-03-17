436cbcd220302fcfb794a77a4fcad347f7ebb3fe
Ryan Badour <rybadour@gmail.com>
2022-03-17 00:33:24 -0400

    Added a basic map with clickable spaces

--- src/components/exploration/exploration.tsx
+++ src/components/exploration/exploration.tsx
@@ -3,8 +3,17 @@ import { useCallback, useContext, useEffect } from "react";
 import { ExplorationContext } from "../../contexts/exploration";
 import { InventoryContext } from "../../contexts/inventory";
 import { startExploring } from "../../game-logic/exploration";
+import { range } from "../../shared/utils";
 import './exploration.css';
 
+const rooms = [{
+  image: 'Basic2x3Room.png',
+  x: 1,
+  y: 1,
+  width: 2,
+  height: 3,
+}];
+const spaceSize = 32;
 
 export function Exploration() {
   const exploration = useContext(ExplorationContext);
@@ -25,5 +34,22 @@ export function Exploration() {
       </> :
       <button onClick={onExplore}>Explore</button>
     )}
+
+    <div className="map">
+      {rooms.map(room =>
+        <div className="room" style={{
+          backgroundImage: `url(${room.image})`,
+          left: room.x * spaceSize,
+          top: room.y * spaceSize,
+          width: room.width * spaceSize,
+          height: room.height * spaceSize,
+        }}>
+          {range(1, room.width).flatMap(x => 
+            range(1, room.height).map(y =>
+              <div className="space"></div>
+          ))}
+        </div>
+      )}
+    </div>
   </div>;
 }
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -2,3 +2,7 @@ export function randomBetween(min: number, max: number) {
   const diff = Math.abs(max - min);
   return Math.random() * diff + Math.min(min, max);
 }
+
+export function range(start: number, end: number): number[] {
+  return Array(end - start + 1).fill(0).map((_, idx) => start + idx)
+}
